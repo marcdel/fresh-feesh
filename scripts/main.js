4f52d0f522c13619dc94dd831514ee446d63cbe5
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-01-31 21:23:15 -0800

    Add router and not found component.

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
 var App = React.createClass({
   render: function(){
     return (
@@ -62,4 +68,20 @@ var StorePicker = React.createClass({
   }
 });
 
-ReactDOM.render(<App />, document.querySelector('#main'));
+var NotFound = React.createClass({
+  render: function(){
+    return (
+      <h1>Not Found!</h1>
+    );
+  }
+});
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={StorePicker} />
+    <Route path="/store/:storeId" component={App} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
+
+ReactDOM.render(routes, document.querySelector('#main'));

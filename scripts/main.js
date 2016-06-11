29b450827b4eb829b18b2e6585fe57055b7dc3ff
Marc Delagrammatikas <mdelagrammatikas@truecar.com>
2016-06-10 17:14:25 -0700

    Chapter 13: Add to order updates state.order

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,10 @@ var App = React.createClass({
       order: {}
     };
   },
+  addToOrder: function(key){
+    this.state.order[key] = this.state.order[key] + 1 || 1;
+    this.setState({ order: this.state.order });
+  },
   addFish: function(fish){
     var timestamp = (new Date()).getTime();
     this.state.fishes['fish-' + timestamp] = fish;
@@ -27,7 +31,7 @@ var App = React.createClass({
     });
   },
   renderFish: function(key){
-    return <Fish key={key} index={key} details={this.state.fishes[key]} />
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />
   },
   render: function(){
     return (
@@ -46,6 +50,10 @@ var App = React.createClass({
 });
 
 var Fish = React.createClass({
+  onButtonClick: function(){
+    var key = this.props.index;
+    this.props.addToOrder(key);
+  },
   render: function(){
     var details = this.props.details;
     var isAvailable = (details.status === 'available' ? true : false);
@@ -59,7 +67,7 @@ var Fish = React.createClass({
           <span className="price">{h.formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button disabled={!isAvailable}>{buttonText}</button>
+        <button disabled={!isAvailable} onClick={this.onButtonClick}>{buttonText}</button>
       </li>
     );
   }
